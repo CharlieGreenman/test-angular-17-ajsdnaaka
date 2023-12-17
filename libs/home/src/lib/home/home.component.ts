e97a5dac75c27d4cc2275fab9102f07432b1328c
Charlie Greenman <charlie@razroo.com>
2023-12-17 10:33:09 -0800

    Add code for angular-17.0.0 add-homepage-component.
    [Url: razroo.com/community/angular-17.0.0/add-homepage/add-homepage-component]

---
+++ libs/home/src/lib/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test-angular-17-ajsdnaaka-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
